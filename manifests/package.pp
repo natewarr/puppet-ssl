de30a4206ce07b6953c54524bd99c3dc89ed75d4
Aaron Russo <aaron.n.russo@gmail.com>
2014-05-17 19:29:43 -0700

    (fixup) called the wrong variable in ssl::packages

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,5 +1,5 @@
 # == Class: ssl::package
 # Installs the necessary packages for the SSL class to function properly
 class ssl::package inherits ssl::params {
-  ensure_packages( flatten([ $::ssl::params::packages ]) )
+  ensure_packages( flatten([ $::ssl::params::package ]) )
 }
